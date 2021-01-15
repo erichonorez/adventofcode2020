e0529d0b738687cd04911f343cf38ffe058aa195
Eric Honorez <eric.honorez@gmail.com>
2021-01-15 22:04:54 +0100

    day6.clj refactored

--- src/adventofcode2020/day6.clj
+++ src/adventofcode2020/day6.clj
@@ -1,32 +1,26 @@
 ;; Day 6
-(ns adventofcode.day6
+(ns adventofcode2020.day6
   (:require [clojure.string :as str]))
 
-(defn group-yes [answers acc]
-  (if (< (count answers) 1)
-    acc
-    (group-yes
-     (rest answers)
-     (clojure.set/union acc (set (first answers))))))
+(defn all-yes [answers]
+  (apply clojure.set/intersection answers))
 
-(defn groups-sum-count-yes [groups]
-  (->> groups
-       (map #(group-yes % []))
+(defn any-yes [answers]
+  (apply clojure.set/union answers))
+
+(defn compute [fn groups]
+  (->> (flatten groups)
+       (#(map fn %))
        (map count)
        (reduce +)))
 
-(defn yes-for-all [answers]
-  (apply clojure.set/intersection
-         (->> answers
-              (map #(set %)))))
-
 (defn parse-file [acc-groups curr-group content]
   (if (< (count content) 1)
-    (conj acc-groups curr-group)
+    (conj acc-groups (map set curr-group))
     (let [x (first content)
           xs (rest content)]
       (if (empty? x)
-        (parse-file (conj acc-groups curr-group) '() xs)
+        (parse-file (conj acc-groups (map set curr-group)) [] xs)
         (parse-file acc-groups (conj curr-group x) xs)))))
 
 (defn read-file [file-name]
@@ -36,14 +30,15 @@
 
 (defn execute-example! []
   (->> (read-file "/Users/eric/Developments/adventofcode2020/resources/day6/example.txt")
-       (#(groups-sum-count-yes %))))
+       (#(parse-file [] [] %))
+       (#(compute any-yes %))))
 
 (defn execute-part-1! []
   (->> (read-file "/Users/eric/Developments/adventofcode2020/resources/day6/input.txt")
-       (#(groups-sum-count-yes %))))
+       (#(parse-file [] [] %))
+       (#(compute any-yes %))))
 
 (defn execute-part-2! []
   (->> (read-file "/Users/eric/Developments/adventofcode2020/resources/day6/input.txt")
-       (map #(yes-for-all %))
-       (map count)
-       (reduce +)))
+       (#(parse-file [] [] %))
+       (#(compute all-yes %))))
