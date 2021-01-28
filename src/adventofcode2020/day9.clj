8a1d3ead3d14d6812ce789102caefda2367c37d1
Eric Honorez <eric.honorez@gmail.com>
2021-01-28 08:21:02 +0100

    Day 9 part 2 completed

--- src/adventofcode2020/day9.clj
+++ src/adventofcode2020/day9.clj
@@ -16,7 +16,7 @@
                                               (rest coll))
     :else (let [pairs (for [x previous-coll
                             y previous-coll
-                            :when (= (+ x y) current)]
+                            :when (and (< x y) (= (+ x y) current))]
                         [x y])]
             (if (= 0 (count pairs))
               current
@@ -37,3 +37,29 @@
        (map #(Long/parseLong %))
        (apply vector)
        (#(find-not-sum-of-previous 25 %))))
+
+(defn sum-first-and-last
+  [v]
+  (+ (apply min v) (apply max v)))
+
+(defn find-contiguous-numbers'
+  [nbr contiguous coll]
+   (let [sum (reduce + contiguous)]
+     (cond
+       (= sum nbr) contiguous
+       (< sum nbr) (find-contiguous-numbers' nbr (conj contiguous (first coll)) (rest coll))
+       (> sum nbr) (find-contiguous-numbers' nbr (subvec contiguous 1) coll))))
+
+(defn find-contiguous-numbers
+  [nbr coll]
+  (sum-first-and-last (find-contiguous-numbers' nbr [] coll)))
+
+(defn execute-part-2
+  []
+  (let [coll (->> (slurp "/Users/eric/Developments/adventofcode2020/resources/day9/input.txt")
+                  str/split-lines
+                  (map #(Long/parseLong %))
+                  (apply vector))]
+    (->> coll
+         (#(find-not-sum-of-previous 25 %))
+         (#(find-contiguous-numbers % coll)))))
--- test/adventofcode2020/day9_test.clj
+++ test/adventofcode2020/day9_test.clj
@@ -7,3 +7,7 @@
 (deftest test-find-not-sum-of-previous
   (is (= 127
          (day9/find-not-sum-of-previous 5 [35 20 15 25 47 40 62 55 65 95 102 117 150 182 127 219 299 277 309 576]))))
+
+(deftest test-find-contiguous-numbers
+  (is (= 62
+         (day9/find-contiguous-numbers 127 [35 20 15 25 47 40 62 55 65 95 102 117 150 182]))))
